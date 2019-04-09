a05badef2aae9a6c9b18e4c648e9fbb4af8df914
Rameshwar.Gholap <Rameshwar.Gholap@CEBS-IT-135.CEBS-CORPORATE.COM>
2019-04-09 18:04:08 +0530

    SpringCloud Service Registration Eureka Server Service One

---
+++ SpringCloudServiceRegistrationEurekaServer_ServieOne/src/main/java/com/spring/cloud/eureka/server/SpringCloudServiceRegistrationEurekaServerServieOneApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServiceRegistrationEurekaServerServieOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServiceRegistrationEurekaServerServieOneApplication.class, args);
+	}
+
+}
